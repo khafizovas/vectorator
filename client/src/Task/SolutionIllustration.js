c2435060bfe6f2e7bfd5ae84cd5da63ff5ddaab3
khafizovas <justhomosapiens121@gmail.com>
2021-10-25 00:29:49 +0300

    Added canvas Added 3d grid drawing

--- client/src/Task/SolutionIllustration.js
+++ client/src/Task/SolutionIllustration.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const SolutionIllustration = (props) => {
-	return <div className='illustration'>It'll be an illustration there!!!</div>;
+	const canvasRef = useRef(null);
+
+	const drawGrid = (ctx, { width, height }) => {
+		ctx.beginPath();
+
+		// OZ
+		ctx.moveTo(0.5 * width, 0);
+		ctx.lineTo(0.5 * width, 0.5 * height);
+
+		// OY
+		ctx.lineTo(width, 0.5 * height);
+
+		// OX
+		ctx.moveTo(0.5 * width, 0.5 * height);
+		ctx.lineTo(0, height);
+
+		ctx.stroke();
+
+		// Captions
+		ctx.fillText('0', 0.5 * width, 0.5 * height);
+		ctx.fillText('x', 0.05, 0.95 * height);
+		ctx.fillText('y', 0.95 * width, 0.5 * height);
+		ctx.fillText('z', 0.5 * width, 0.05 * height);
+	};
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+
+		const context = canvas.getContext('2d');
+		context.font = '12px bold';
+
+		const size = { width: canvas.width, height: canvas.height };
+
+		drawGrid(context, size);
+	}, []);
+
+	return <canvas id='illustration' ref={canvasRef} {...props} />;
 };
 
 export default SolutionIllustration;
