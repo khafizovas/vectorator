f3dcae2799749ff272cb0dd252bd5516130c1f80
khafizovas <justhomosapiens121@gmail.com>
2021-11-05 23:11:19 +0300

    Added tasks routes

--- server/routes/tasks.routes.js
+++ server/routes/tasks.routes.js
@@ -14,4 +14,56 @@ router
 	.route('/api/tasks/build_parallelogram')
 	.post(tasksController.buildParallelogram);
 
+router
+	.route('/api/tasks/find_sides_length')
+	.post(tasksController.findSidesLength);
+
+router
+	.route('/api/tasks/find_angle_between_diagonales')
+	.post(tasksController.findAngleBetweenDiagonales);
+
+router
+	.route('/api/tasks/find_parallelogram_area')
+	.post(tasksController.findParallelogramArea);
+
+router
+	.route('/api/tasks/build_parallelepiped')
+	.post(tasksController.buildParallelepiped);
+
+router
+	.route('/api/tasks/find_parallelepiped_volume')
+	.post(tasksController.findParallelepipedVolume);
+
+router
+	.route('/api/tasks/find_parallelepiped_height')
+	.post(tasksController.findParallelepipedHeight);
+
+router
+	.route('/api/tasks/find_vector_in_basis')
+	.post(tasksController.findVectorInBasis);
+
+router
+	.route('/api/tasks/find_vector_projection')
+	.post(tasksController.findVectorProjection);
+
+router
+	.route('/api/tasks/find_plane_equation_by_points')
+	.post(tasksController.findPlaneEquationByPoints);
+
+router
+	.route('/api/tasks/find_plane_equation_by_point_and_line')
+	.post(tasksController.findPlaneEquationByPointAndLine);
+
+router
+	.route('/api/tasks/find_distance_between_lines')
+	.post(tasksController.findDistanceBetweenLines);
+
+router
+	.route('/api/tasks/find_symmetrical_point')
+	.post(tasksController.findSymmetricalPoint);
+
+router
+	.route('/api/tasks/find_angle_between_planes')
+	.post(tasksController.findAngleBetweenPlanes);
+
 module.exports = router;
