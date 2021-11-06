b1ca1e3c38d8642860437c6cb7bb906a91971d73
khafizovas <justhomosapiens121@gmail.com>
2021-11-06 22:44:33 +0300

    maths.findParallelepipedHeight

---
+++ server/services/maths/find_parallelepiped_height.js
@@ -0,0 +1,28 @@
+const findParallelepipedVolume = require('./find_parallelepiped_volume');
+const findParallelogramArea = require('./find_parallelogram_area');
+
+/**
+ * Найти высоту параллелепипеда, построенного на векторах AB, AD, AA1.
+ * @param {Vector3D} AB
+ * @param {Vector3D} AD
+ * @param {Vector3D} AA1
+ * @returns {solution}
+ */
+const findParallelepipedHeight = (AB, AD, AA1) => {
+	const task = { AB: AB, AD: AD, AA1: AA1 };
+	let solution = findParallelepipedVolume(AB, AD, AA1).solution;
+
+	solution = [...solution, findParallelogramArea(AB, AD).solution];
+
+	solution.push({
+		type: 'number',
+		name: 'H',
+		value: solution[1].value / solution[3].value,
+	});
+
+	result = { type: 'number', value: solution[4].value };
+
+	return { task, solution, result };
+};
+
+module.exports = findParallelepipedHeight;
