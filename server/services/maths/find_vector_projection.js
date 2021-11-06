a2542632c5b7d5e4d473b7a6fde98bd246d08def
khafizovas <justhomosapiens121@gmail.com>
2021-11-07 01:58:09 +0300

    maths.findVectorProjection

---
+++ server/services/maths/find_vector_projection.js
@@ -0,0 +1,37 @@
+const findScalarProduct = require('./helpers/find_scalar_product');
+const findVectorLength = require('./helpers/find_vector_length');
+
+/**
+ * Найти проекцию вектора AH на вектор AA_1
+ * @param {Vector3D} lhs
+ * @param {Vector3D} rhs
+ * @returns {solution}
+ */
+const findVectorProjection = (lhs, rhs) => {
+	const task = { lhs, rhs };
+	const solution = [];
+
+	solution.push({
+		type: 'number',
+		name: 'AH * AA_1',
+		value: findScalarProduct(lhs, rhs),
+	});
+
+	solution.push({
+		type: 'number',
+		name: '|AA_1|',
+		value: findVectorLength(rhs),
+	});
+
+	solution.push({
+		type: 'number',
+		name: 'projection',
+		value: solution[0].value / solution[1].value,
+	});
+
+	const result = { type: 'vector', value: solution[2].value };
+
+	return { task, solution, result };
+};
+
+module.exports = findVectorProjection;
