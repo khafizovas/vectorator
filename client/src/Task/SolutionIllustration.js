068e0b8f3b1ff2b2930fa9634a7879c010a6cb7d
khafizovas <justhomosapiens121@gmail.com>
2021-11-14 17:23:15 +0300

    Fixed unit calculations

--- client/src/Task/SolutionIllustration.js
+++ client/src/Task/SolutionIllustration.js
@@ -426,16 +426,42 @@ const SolutionIllustration = (props) => {
 		context.lineWidth = 2;
 		context.font = '14px Comic Sans MS';
 
+		const findPointRadius = (coordinates) => {
+			return Math.sqrt(
+				coordinates.reduce(
+					(sum, coordinate) => sum + (Math.sqrt(2) * coordinate) ** 2,
+					0
+				)
+			);
+		};
+
 		const unit =
 			(0.5 * size) /
 			Math.max(
 				...[...props.task, ...props.solution]
 					.filter((step) => FIGURE_TYPES.includes(step.type))
-					.map((step) =>
-						Array.isArray(step.value) ? step.value.flat(Infinity) : step.value
-					)
+					.map((step) => {
+						if (Array.isArray(step.value)) {
+							if (Array.isArray(step.value[0])) {
+								if (Array.isArray(step.value[0][0])) {
+									// Parallelepiped
+									return step.value.map((points) =>
+										points.map((point) => findPointRadius(point))
+									);
+								} else {
+									// Parallelogram, plane, vector
+									return step.value.map((point) => findPointRadius(point));
+								}
+							} else {
+								// Point
+								return findPointRadius(step.value);
+							}
+						} else {
+							// Coordinate
+							return Math.sqrt(2) * Math.abs(step.value);
+						}
+					})
 					.flat(Infinity)
-					.map((coordinate) => 2 * Math.sqrt(2) * coordinate)
 			);
 
 		drawGrid(context, size, unit);
